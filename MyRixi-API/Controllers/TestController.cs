701f86122e40f57a656ed3f0055eae4f02dd9e44
ubdaa <123176831+ubdaa@users.noreply.github.com>
2025-02-26 11:57:32 +0100

    accès à l'api partout fonctionnel

---
+++ MyRixi-API/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyRixiApi.Controllers;
+
+public class TestController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
