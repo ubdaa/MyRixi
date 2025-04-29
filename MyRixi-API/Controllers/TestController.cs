7bafdd8ed2c5296ffc490544a3febb00b114ebf8
ubdaa <123176831+ubdaa@users.noreply.github.com>
2025-04-29 17:00:02 +0200

    ajout du endpoint test de mail

--- MyRixi-API/Controllers/TestController.cs
+++ MyRixi-API/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
 using MyRixiApi.Interfaces;
 using MyRixiApi.Utilities;
 
@@ -9,10 +10,12 @@ namespace MyRixiApi.Controllers;
 public class TestController : ControllerBase
 {
     private IStorageService _storageService;
-    
-    public TestController(IStorageService storageService)
+    private readonly IEmailSender _emailSender;
+
+    public TestController(IStorageService storageService, IEmailSender emailSender)
     {
         _storageService = storageService;
+        _emailSender = emailSender;
     }
     
     [HttpGet]
@@ -27,5 +30,16 @@ public class TestController : ControllerBase
         
         var url = ProfilePictureGenerator.GenerateRandomProfilePictureAsync(_storageService, Guid.NewGuid());
         return Ok(new { message = url });
+    }
+
+    [HttpGet("test-mail")]
+    public IActionResult TestMail([FromQuery] string email)
+    {
+        var subject = "Test Email";
+        var htmlMessage = "<h1>Hello</h1><p>This is a test email.</p>";
+        
+        _emailSender.SendEmailAsync(email, subject, htmlMessage);
+        
+        return Ok(new { message = "Email sent" });
     }
 }
